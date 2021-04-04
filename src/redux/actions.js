61537dd6d0fa16441252176b5199e100473d498b
maxbendas <56072735+maxbendas@users.noreply.github.com>
2021-04-04 12:45:59 +0300

    Routing implemented

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,4 @@
-import {CHANGE_TEXT, CHANGE_STYLES, TABLE_RESIZE, APPLY_STYLE, CHANGE_TITLE} from "@/redux/types";
+import {CHANGE_TEXT, CHANGE_STYLES, TABLE_RESIZE, APPLY_STYLE, CHANGE_TITLE, UPDATE_DATE} from "@/redux/types";
 
 export function tableResize(data) {
     return {
@@ -35,4 +35,11 @@ export function changeTitle(data) {
         data
     }
 }
+
+export function updateDate() {
+    return {
+        type: UPDATE_DATE
+    }
+}
+
 
